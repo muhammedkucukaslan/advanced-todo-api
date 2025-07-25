1a1ae3e3a711ecd01e8570dbafe84c2a67bfaef5
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-07-25 18:51:41 +0300

    feat: add healthcheck

---
+++ app/healthcheck/healthcheck.go
@@ -0,0 +1,31 @@
+package healthcheck
+
+import "context"
+
+type HealthcheckRequest struct{}
+
+type HealthcheckResponse struct {
+	Status string `json:"status" example:"OK"`
+}
+
+type HealthcheckHandler struct{}
+
+// NewHealthcheckHandler creates a new HealthcheckHandler instance.
+//
+//	@Summary		Healthcheck
+//	@Description	Check the health of the service
+//	@Tags			1- Healthcheck
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	HealthcheckResponse
+//	@Failure		500	{object}	domain.Error
+//	@Router			/healthcheck [get]
+func NewHealthcheckHandler() *HealthcheckHandler {
+	return &HealthcheckHandler{}
+}
+
+func (h *HealthcheckHandler) Handle(ctx context.Context, req *HealthcheckRequest) (*HealthcheckResponse, int, error) {
+	return &HealthcheckResponse{
+		Status: "OK",
+	}, 200, nil
+}
