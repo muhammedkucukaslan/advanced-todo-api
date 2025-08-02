4ef6d0650699710ad49de9836abe6fb3e9e5ffdd
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-08-02 19:32:40 +0300

    feat: add integration testing for forgot password handler

--- app/user/forgot_password_handler.go
+++ app/user/forgot_password_handler.go
@@ -33,24 +33,29 @@ func NewForgotPasswordHandler(repo Repository, emailService MailService, tokenSe
 
 // Handle processes the request to initiate a password reset.
 //
-//	@Summary		Forgot Password
-//	@Description	It sends  a password reset link to the user's email address.
-//	@Tags			3- User
-//	@Accept			json
-//	@Produce		json
-//	@Param			response-language	header	string					true	"Response Language"	enums(tr,ar,en)
-//	@Param			request				body	ForgotPasswordRequest	true	"Forgot Password Request"
-//	@Success		204
-//	@Failure		400
-//	@Failure		500
-//	@Router			/users/forgot-password [post]
+//		@Summary		Forgot Password
+//		@Description	It sends  a password reset link to the user's email address.
+//		@Tags			3- User
+//		@Accept			json
+//		@Produce		json
+//		@Param			response-language	header	string					true	"Response Language"	enums(tr,ar,en)
+//		@Param			request				body	ForgotPasswordRequest	true	"Forgot Password Request"
+//		@Success		204
+//		@Failure		400
+//	 @Failure		401
+//		@Failure		500
+//		@Router			/users/forgot-password [post]
 func (h *ForgotPasswordHandler) Handle(ctx context.Context, req *ForgotPasswordRequest) (*ForgotPasswordResponse, int, error) {
 	if err := h.validator.Validate(req); err != nil {
 		return nil, http.StatusBadRequest, domain.ErrInvalidRequest
 	}
 
-	if exists, _ := h.repo.EmailExists(ctx, req.Email); !exists {
-		return nil, http.StatusOK, nil
+	if exists, err := h.repo.EmailExists(ctx, req.Email); err != nil {
+		return nil, http.StatusInternalServerError, domain.ErrInternalServer
+	} else if !exists {
+		// If the email does not exist, we still return a success response
+		// to prevent email enumeration attacks.
+		return nil, http.StatusNoContent, nil
 	}
 
 	token, err := h.tokenService.GenerateTokenForForgotPassword(req.Email)
