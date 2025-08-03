b06091fd16fb10d16eef772d41b4c565ca440037
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-08-03 14:16:07 +0300

    feat: add http testing  for auth middleware

---
+++ tests/httptest/middleware/auth_middleware_test.go
@@ -0,0 +1,136 @@
+package httptest_middleware
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/muhammedkucukaslan/advanced-todo-api/app/healthcheck"
+	"github.com/muhammedkucukaslan/advanced-todo-api/domain"
+	fiberInfra "github.com/muhammedkucukaslan/advanced-todo-api/infrastructure/fiber"
+	"github.com/muhammedkucukaslan/advanced-todo-api/infrastructure/jwt"
+	"github.com/muhammedkucukaslan/advanced-todo-api/infrastructure/slog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAuthMiddleware(t *testing.T) {
+	app := fiber.New()
+
+	realTokenService := jwt.NewTokenService(domain.MockJWTTestKey, time.Hour*24, time.Minute*10, time.Minute*10)
+	fakeTokenService := jwt.NewTokenService("fake-key", time.Hour*24, time.Minute*10, time.Minute*10)
+
+	logger := slog.NewLogger()
+	middlewareManager := fiberInfra.NewMiddlewareManager(realTokenService, logger)
+
+	healthCheckHandler := healthcheck.NewHealthcheckHandler()
+	app.Get("/healthcheck", middlewareManager.AuthMiddleware, fiberInfra.Handle(healthCheckHandler, logger))
+
+	validToken, err := realTokenService.GenerateToken(domain.RealUserId, domain.TestUser.Role, time.Now())
+	require.NoError(t, err, "failed to generate valid token")
+
+	fakeToken, err := fakeTokenService.GenerateToken(domain.FakeUserId, domain.TestUser.Role, time.Now())
+	require.NoError(t, err, "failed to generate fake token")
+
+	expiredToken, err := realTokenService.GenerateToken(domain.RealUserId, domain.TestUser.Role, time.Now().Add(-time.Hour*100))
+	require.NoError(t, err, "failed to generate expired token")
+
+	validTokenHeader := "Bearer " + validToken
+	fakeTokenHeader := "Bearer " + fakeToken
+	invalidTokenHeader := "Bearer " + "invalid_token"
+	invalidHeader := " Bearerrrrrr " + validToken
+	missingTokenHeader := "Bearer "
+	expiredTokenHeader := "Bearer " + expiredToken
+
+	type args struct {
+		authHeader string
+		req        *healthcheck.HealthcheckRequest
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *healthcheck.HealthcheckResponse
+		code    int
+		wantErr error
+	}{
+		{
+			"valid request", args{
+				authHeader: validTokenHeader,
+				req:        &healthcheck.HealthcheckRequest{},
+			}, &healthcheck.HealthcheckResponse{
+				Status: healthcheck.IamAlive,
+			}, http.StatusOK, nil,
+		},
+		{
+			"invalid token", args{
+				authHeader: invalidTokenHeader,
+				req:        &healthcheck.HealthcheckRequest{},
+			}, nil, http.StatusUnauthorized, domain.ErrInvalidToken,
+		},
+		{
+			"fake token", args{
+				authHeader: fakeTokenHeader,
+				req:        &healthcheck.HealthcheckRequest{},
+			}, nil, http.StatusUnauthorized, domain.ErrInvalidToken,
+		},
+		{
+			"expired token", args{
+				authHeader: expiredTokenHeader,
+				req:        &healthcheck.HealthcheckRequest{},
+			}, nil, http.StatusUnauthorized, domain.ErrInvalidToken,
+		},
+		{
+			"invalid header", args{
+				authHeader: invalidHeader,
+				req:        &healthcheck.HealthcheckRequest{},
+			}, nil,
+			http.StatusUnauthorized, domain.ErrInvalidAuthHeader,
+		},
+		{
+			"missing header", args{
+				req: &healthcheck.HealthcheckRequest{},
+			}, nil, http.StatusUnauthorized, domain.ErrMissingAuthHeader,
+		},
+		{
+			"missing token", args{
+				authHeader: missingTokenHeader,
+				req:        &healthcheck.HealthcheckRequest{},
+			}, nil, http.StatusUnauthorized, domain.ErrInvalidAuthHeader,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, "/healthcheck", nil)
+			require.NoError(t, err)
+
+			if tt.args.authHeader != "" {
+				req.Header.Set("Authorization", tt.args.authHeader)
+			}
+
+			resp, err := app.Test(req, -1)
+			require.NoError(t, err, "failed to create request")
+			require.NotNil(t, resp)
+			defer resp.Body.Close()
+
+			require.Equal(t, tt.code, resp.StatusCode)
+			if tt.code >= 400 {
+				assert.NotEmpty(t, resp.Body, "response body should not be empty for error cases")
+				var errResp domain.Error
+				err = json.NewDecoder(resp.Body).Decode(&errResp)
+				require.NoError(t, err)
+				assert.NotEmpty(t, errResp.Message, "error message should not be empty")
+				assert.Equal(t, errResp.Message, tt.wantErr.Error())
+				assert.Equal(t, errResp.Code, tt.code)
+			} else {
+				var res healthcheck.HealthcheckResponse
+				err = json.NewDecoder(resp.Body).Decode(&res)
+				require.NoError(t, err, "failed to decode response")
+				assert.NotNil(t, res, "response should not be nil")
+				assert.Equal(t, tt.want.Status, res.Status, "response status should match")
+			}
+		})
+	}
+}
