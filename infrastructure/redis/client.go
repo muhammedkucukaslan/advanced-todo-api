887f9a241e9ac7a9679a8c19a06e2ebc92cb8b0e
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-08-09 21:36:26 +0300

    feat: add cache interface and implement  redis package

---
+++ infrastructure/redis/client.go
@@ -0,0 +1,51 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func NewRedisClient() *RedisClient {
+
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	client := redis.NewClient(opt)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		panic(fmt.Sprintf("Redis connection failed: %v", err))
+	}
+
+	return &RedisClient{
+		client: client,
+	}
+}
+
+func (r *RedisClient) Get(ctx context.Context, key string) ([]byte, error) {
+	val, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+	return []byte(val), nil
+}
+
+func (r *RedisClient) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (r *RedisClient) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
