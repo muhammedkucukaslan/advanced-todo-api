54f5ef23900d3e42aac8024eb838274ebdd5aa83
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-08-13 16:22:53 +0300

    feat(auth): add GenerateAuthRefreshToken func

--- app/auth/token_service.go
+++ app/auth/token_service.go
@@ -9,4 +9,5 @@ type TokenService interface {
 	GenerateAuthAccessToken(userID string, role string) (string, error)
 	ValidateAuthAccessToken(token string) (*TokenPayload, error)
 	GenerateEmailVerificationToken(email string) (string, error)
+	GenerateAuthRefreshToken(userID string, role string) (string, error)
 }
---
+++ domain/refresh_token.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type RefreshToken struct {
+	Id        uuid.UUID `json:"id"`
+	UserID    uuid.UUID `json:"user_id"`
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func NewRefreshToken(userID uuid.UUID, token string, duration time.Duration) *RefreshToken {
+	return &RefreshToken{
+		Id:        uuid.New(),
+		UserID:    userID,
+		Token:     token,
+		ExpiresAt: time.Now().Add(duration),
+		CreatedAt: time.Now(),
+	}
+}
