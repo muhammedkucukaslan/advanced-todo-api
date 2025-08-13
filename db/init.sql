ac2329ae64be28b146dd7a69e09674c618a3f3ec
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2025-08-13 14:27:22 +0300

    feat: add refresh token table

--- db/init.sql
+++ db/init.sql
@@ -16,3 +16,11 @@ CREATE TABLE todos (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   completed_at TIMESTAMP DEFAULT NULL
 );
+
+CREATE TABLE refresh_tokens (
+    id              UUID PRIMARY KEY,
+    user_id         UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token           TEXT NOT NULL UNIQUE,
+    expires_at      TIMESTAMP NOT NULL,
+    created_at      TIMESTAMP NOT NULL DEFAULT NOW()
+);
